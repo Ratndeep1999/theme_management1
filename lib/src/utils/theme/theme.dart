3d9421e9bc90bff589aacf657e30206ca4ae01b9
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-13 19:44:00 +0530

    theme.dart: MyAppTheme() class add, static ThemeData kLightTheme = ThemeData.light(), and static ThemeData kDarkTheme = ThemeData.dark() add.

---
+++ lib/src/utils/theme/theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MyAppTheme {
+  /// Light Theme
+  static ThemeData kLightTheme = ThemeData.light();
+
+  /// Dark Theme
+  static ThemeData kDarkTheme = ThemeData.dark();
+}
