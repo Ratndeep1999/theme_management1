7c9566fa4cf9244c4447fff2c52d0fb98d3c5531
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-13 20:48:53 +0530

    text_styles.dart: text styles add.

---
+++ lib/src/utils/theme/text_styles.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TextStyles {
+  TextStyles._();
+
+  /// Display styles
+  static TextStyle kBoldDisplay57w700 = GoogleFonts.poppins(
+    fontSize: 57,
+    fontWeight: FontWeight.w700,
+    color: Colors.white,
+  );
+
+  static TextStyle kBoldDisplay45w700 = GoogleFonts.poppins(
+    fontSize: 45,
+    fontWeight: FontWeight.w700,
+    color: Colors.white,
+  );
+
+  static TextStyle kBoldDisplay36w700 = GoogleFonts.poppins(
+    fontSize: 36,
+    fontWeight: FontWeight.w700,
+    color: Colors.white,
+  );
+
+  /// Headline styles
+  static TextStyle kBoldHeadline32w700 = GoogleFonts.poppins(
+    fontSize: 32,
+    fontWeight: FontWeight.w700,
+    color: Colors.white,
+  );
+
+  static TextStyle kSemiBoldHeadline28w600 = GoogleFonts.poppins(
+    fontSize: 28,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
+
+  static TextStyle kSemiBoldHeadline24w600 = GoogleFonts.poppins(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
+
+  /// Title styles
+  static TextStyle kSemiBoldTitle22w600 = GoogleFonts.poppins(
+    fontSize: 22,
+    fontWeight: FontWeight.w600,
+    color: Colors.white,
+  );
+
+  static TextStyle kMediumTitle16w500 = GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+
+  static TextStyle kMediumTitle14w500 = GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+
+  /// Body styles
+  static TextStyle kRegularBody16w400 = GoogleFonts.poppins(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+
+  static TextStyle kRegularBody14w400 = GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+
+  static TextStyle kRegularBody12w400 = GoogleFonts.poppins(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: Colors.white,
+  );
+
+  /// Label styles
+  static TextStyle kMediumLabel14w500 = GoogleFonts.poppins(
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+
+  static TextStyle kMediumLabel12w500 = GoogleFonts.poppins(
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+
+  static TextStyle kMediumLabel11w500 = GoogleFonts.poppins(
+    fontSize: 11,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+  );
+}
