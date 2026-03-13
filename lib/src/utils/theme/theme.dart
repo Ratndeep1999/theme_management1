826c379257074c3033b26bd86dc2c9d017a4d686
Ratndeep Chandankhede <ratndeepchandankhede1@gmail.com>
2026-03-13 20:15:53 +0530

    theme.dart: textTheme: AppTextTheme.lightTextTheme add, and textTheme: AppTextTheme.darkTextTheme add.

--- lib/src/utils/theme/theme.dart
+++ lib/src/utils/theme/theme.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widget_themes/app_text_theme.dart';
+
 class MyAppTheme {
   /// Private Constructor
   MyAppTheme._();
@@ -7,37 +9,12 @@ class MyAppTheme {
   /// Light Theme
   static final ThemeData kLightTheme = ThemeData(
     brightness: Brightness.light,
-    textTheme: const TextTheme(
-      /// Display (Very large headings)
-      displayLarge: TextStyle(fontSize: 57, fontWeight: FontWeight.bold),
-      displayMedium: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
-      displaySmall: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-
-      /// Headline (Section headings)
-      headlineLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
-      headlineMedium: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
-      headlineSmall: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-
-      /// Title (AppBar / Card titles)
-      titleLarge: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-      titleMedium: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
-      titleSmall: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-
-      /// Body (Normal text)
-      bodyLarge: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-      bodyMedium: TextStyle(fontSize: 14, fontWeight: FontWeight.w400),
-      bodySmall: TextStyle(fontSize: 12, fontWeight: FontWeight.w400),
-
-      /// Label (Buttons / captions)
-      labelLarge: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-      labelMedium: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
-      labelSmall: TextStyle(fontSize: 11, fontWeight: FontWeight.w500),
-    ),
+    textTheme: AppTextTheme.lightTextTheme,
   );
 
   /// Dark Theme
   static final ThemeData kDarkTheme = ThemeData(
     brightness: Brightness.dark,
-    textTheme: kLightTheme.textTheme,
+    textTheme: AppTextTheme.darkTextTheme,
   );
 }
